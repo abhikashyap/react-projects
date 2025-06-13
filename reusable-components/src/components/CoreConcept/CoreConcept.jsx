e04aa21cf4ae6b3a5ace785f05e44eee44b75108
abhikashyap <abhikashyap.ds@gmail.com>
2025-06-13 19:27:36 +0530

    implemented hook and children

---
+++ reusable-components/src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,13 @@
+import './CoreConcept.css'
+export default function CoreConcept({image,title,description}){
+  return(
+  <li>
+    <img src={image} alt={title} />
+    <h1>{title}</h1>
+    <p>
+      {description}
+    </p>
+  </li>
+  )
+
+}
---
+++ reusable-components/src/components/Tab/Tab.jsx
@@ -0,0 +1,8 @@
+import './Tab.css'
+export default function TabButton({children,onSelect}){
+    return (
+        <li>
+            <button onClick={onSelect}>{children}</button>
+        </li>
+    )
+}

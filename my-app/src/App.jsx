9da8bddc7d410ed3afd104983db1622b1ed5d2bc
abhikashyap <abhikashyap.ds@gmail.com>
2025-06-13 12:31:15 +0530

    hellow world - create and use component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,9 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import cat from './assets/cat.jpeg' // refrence in src/asset with vcariable name
+function User(){
+  return (
+    <>
+    <p>
+      abhi
+    </p>
+    </>
+  )
 
+
+}
 function Greetings(){
-  return <p>welcome to the world</p>
+
+  return (
+    <>
+      <p>welcome to the world</p>
+      <p>my name is {User()}</p>
+      <img src={cat} />
+      {/* reference from prublic folder */}
+      <img src="/vite.svg"/>  
+      
+    </>
+
+)
 }
 
 function App() {
